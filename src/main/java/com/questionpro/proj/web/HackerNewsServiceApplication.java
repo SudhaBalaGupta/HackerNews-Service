baf41d2b9932478cffee16f57c0638163f3116ed
Sudha <sudhagupta.speaks@gmail.com>
2023-06-13 13:25:24 +0530

    Initial project structure

---
+++ src/main/java/com/questionpro/proj/web/HackerNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.questionpro.proj.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerNewsServiceApplication.class, args);
+	}
+
+}
